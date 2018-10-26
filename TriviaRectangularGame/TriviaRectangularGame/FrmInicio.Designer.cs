c5e4f1125d73d0eb276d510d0bf19fcbbadd971e
Alan Castro <alan.castrofr@uanl.edu.mx>
2018-10-26 17:54:28 -0500

    Creacion de formulaciones principales
    creacion de formularios principales
    +inicio
    +dados
    +preguntas

---
+++ TriviaRectangularGame/TriviaRectangularGame/FrmInicio.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace TriviaRectangularGame
+{
+    partial class FrmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 282);
+            this.Name = "FrmInicio";
+            this.Text = "TriviaRectangularGame";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
